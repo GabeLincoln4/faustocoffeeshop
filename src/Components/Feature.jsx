0cd0d951152c7cf245ce3eccac7b8f950f74a08a
Gavin <7797864896@K12.LCS>
2022-03-08 10:24:11 -0500

    Remove AOS init and data-aos

--- src/Components/Feature.jsx
+++ src/Components/Feature.jsx
@@ -35,7 +35,7 @@ function Feature(){
 
     return (
         <div className="bg-yellow-100 h-full" id="home">
-            <div className="grid grid-cols-1 md:grid-cols-2 md:grid-rows-2 p-20" data-aos="zoom-in">
+            <div className="grid grid-cols-1 md:grid-cols-2 md:grid-rows-2 p-20" data-aos="">
                         <div className="md:p-4 text-center md:self-end h-2/3">
                             <h1 className="md:text-left text-6xl md:text-7xl leading-tight">Freshly <span className="text-yellow-800" whileHover="text-white">Roasted. </span>Downtown <span className="text-yellow-400">Feel.</span></h1>
                         </div>
