48438e56cf5600343d5110037d74aadef5fe996d
Gavin <7797864896@K12.LCS>
2022-03-09 10:32:14 -0500

    Add AOS library and initialize it in the useEffect React Hook

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Route, Link} from "react-router-dom";
 import Home from "./Home.jsx";
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function App() {
+
+  useEffect(() => {
+    AOS.init();
+  })
+
   return (
     <div>
       <Route exact path="/" component={Home} />
